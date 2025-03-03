1d32cf8997a3d8fbcb28cd597c2962d1255c0b8f
stheven4496 <stheven4496@gmail.com>
2025-03-03 01:42:04 +0000

    another commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Opening from './content/opening';
 import Pesan from './content/pesan';
